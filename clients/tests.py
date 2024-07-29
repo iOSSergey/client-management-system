d1aa79cee1d7c81c8acdc7ec54335a29eea7d9f9
iOSSergey <live.on@hotmail.com>
2024-07-29 06:10:04 +0300

    :sparkles: Add unit tests for Latest Clients View

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Client
 
-# Create your tests here.
+class ClientViewsTest(TestCase):
+    def setUp(self):
+        # Create test data
+        Client.objects.create(first_name="John", last_name="Doe")
+        Client.objects.create(first_name="Jane", last_name="Doe")
+        # Add more clients for the test
+
+    def test_latest_clients_view(self):
+        response = self.client.get(reverse('latest_clients'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clients/latest_clients.html')
+        self.assertTrue('clients' in response.context)
+        # Check that no more than 5 clients are returned
+        self.assertLessEqual(len(response.context['clients']), 5)
