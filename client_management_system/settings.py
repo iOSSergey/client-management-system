3889a8a97ed1021aa4e7e3b76a74feae789d57ad
iOSSergey <live.on@hotmail.com>
2024-08-01 19:01:52 +0300

    :sparkles: Minor Update in settings.py

--- client_management_system/settings.py
+++ client_management_system/settings.py
@@ -37,7 +37,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = ['pavlyuk.online', 'www.pavlyuk.online']
+ALLOWED_HOSTS = ['127.0.0.1', 'pavlyuk.online', 'www.pavlyuk.online']
 
 
 # Application definition
