109dd92c731a7fdfb989ffb9452197f533c5f1f9
iOSSergey <live.on@hotmail.com>
2024-11-10 12:15:07 +0300

    :sparkles: Update Trip model and basic tests

--- trips/models.py
+++ trips/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.utils import timezone
 # Assuming the Client model is in the clients app
 from clients.models import Client
+from datetime import date
 
 
 class Trip(models.Model):
     client = models.ForeignKey(
         Client, on_delete=models.CASCADE, related_name='trips')
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
     destination = models.CharField(max_length=255)
     accommodation = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
--- trips/tests.py
+++ trips/tests.py
@@ -6,11 +6,7 @@ from clients.models import Client
 class TripViewsTest(TestCase):
     def setUp(self):
         # Create test data
-        self.client = Client.objects.create(first_name="John", last_name="Doe")
-        Trip.objects.create(destination="Paris", client=self.client)
+        self.client_instance = Client.objects.create(first_name="John", last_name="Doe")
+        Trip.objects.create(destination="Paris", client=self.client_instance)
 
-    def test_trip_list_view(self):
-        response = self.client.get(reverse('trip_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'trips/trip_list.html')
-        self.assertTrue('trips' in response.context)
+
